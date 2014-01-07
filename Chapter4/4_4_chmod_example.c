35858bb3aa953db041227c00da953611a986ca86
MaximusZhou <zhougang009@gmail.com>
2014-01-07 17:10:15 +0800

    modify comment

--- Chapter4/4_4_chmod_example.c
+++ Chapter4/4_4_chmod_example.c
@@ -23,7 +23,7 @@ int main(void)
 	if (chmod("foo", (statbuf.st_mode & ~S_IXGRP) | S_ISGID) < 0)
 		err_sys("chmod error for foo");
 
-	/*set absolute mode to "rw-r--r--" */
+	/*set absolute mode to "rw-r--r--"*/
 	if (chmod("bar",S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH))
 		err_sys("chmod error for bar");
 	
