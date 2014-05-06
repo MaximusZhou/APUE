0fa78449de22215e1a24fa4cb72c3389652acc40
MaximusZhou <zhougang009@gmail.com>
2014-05-06 22:14:39 +0800

    print process environment

--- Chapter7/7_2_atexit.c
+++ Chapter7/7_2_atexit.c
@@ -3,23 +3,40 @@
 	Example:
 	$gcc -Wall 7_2_atexit.c ../lib/error.c -o 7_2_atexit
 	$./7_2_atexit 
+	************************************
+	*********output environment*********
+	************************************
 	first exit handle
 	first exit handle
 	second exit handle
 
 	Analyse:
+	The exit function first calls the exit handles and then closes(via fclose) all open streams.
 	The exit function calls these function in reverse order of their registation.
 	Each function is called as many times as it was registed.
 */
 
 #include "../include/apue.h"
-#include <pwd.h>
 
 static void my_exit1(void);
 static void my_exit2(void);
 
+extern char **environ;  /*environment pointer*/
+
 int main(void)
 {
+	printf("begin output environment\n");
+	
+	char **p = environ;
+
+	while(*p != NULL)
+	{
+		printf("%s\n",*p);
+		p++;
+	}
+
+	printf("end output environment\n");
+
 	if (atexit(my_exit2) != 0)
 		err_sys("can't register my_exit2");
 
